a97600652d967e5b560833eb788d1399e2bba76e
jhonatan1701621476 <jhonatan.1701621476@ucaldas.edu.co>
2024-04-07 18:16:09 -0500

    estrategia de autorizacion primera parte

---
+++ src/auth/strategy.ts
@@ -0,0 +1,17 @@
+import {AuthenticationStrategy} from '@loopback/authentication';
+import {Request} from '@loopback/rest';
+import {UserProfile} from '@loopback/security';
+
+export class AuthStrategy implements AuthenticationStrategy {
+  name: string = 'auth';
+
+  constructor() { }
+
+  async authenticate(request: Request): Promise<UserProfile | undefined> {
+
+    console.log("ejecutando estrategia de autenticación básica")
+
+    return undefined
+  }
+
+}
