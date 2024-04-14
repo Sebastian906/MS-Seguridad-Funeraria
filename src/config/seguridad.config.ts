392165d3d7c4fa7a65c4d4a50d53baebc5d475d2
Sebastian <sebastian.1701811396@ucaldas.edu.co>
2024-04-14 17:38:04 -0500

    Configuración de las variables de entorno con Loopback

--- src/config/seguridad.config.ts
+++ src/config/seguridad.config.ts
@@ -1,9 +1,10 @@
 export namespace ConfiguracionSeguridad {
-  export const claveJWT: string = "Admin4";
+  export const claveJWT= process.env.SECRET_PASSWORD_JWT;
   export const menuUsuarioId = "660c36a5f2d17933082d1122";
   export const listarAccion = "Listar";
   export const guardarAccion = "Guardar";
   export const editarAccion = "Editar";
   export const eliminarAccion = "Eliminar";
   export const descargarAccion = "Descargar";
+  export const mongodbConnectionString = process.env.CONNECTION_STRING_MONGODB;
 }
