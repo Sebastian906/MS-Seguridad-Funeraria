62622aa286600e063c4cbd22905821d840c69771
Daniel Cardona <carlos.cardona28426@ucaldas.edu.co>
2024-06-11 17:07:34 -0500

    Eliminación pqrs

--- src/models/index.ts
+++ src/models/index.ts
@@ -1,10 +1,10 @@
-export * from './usuario.model';
-export * from './login.model';
-export * from './rol.model';
-export * from './menu.model';
-export * from './rol-menu.model';
 export * from './credenciales.model';
 export * from './factor-de-autenticacion-por-codigo.model';
-export * from './permisos-rol-menu.model';
 export * from './hash-validacion-usuario.model';
-export * from './pqrs.model';
+export * from './login.model';
+export * from './menu.model';
+export * from './permisos-rol-menu.model';
+export * from './rol-menu.model';
+export * from './rol.model';
+export * from './usuario.model';
+
--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -3,9 +3,4 @@ export * from './menu.repository';
 export * from './rol-menu.repository';
 export * from './rol.repository';
 export * from './usuario.repository';
-export * from './login.repository';
-export * from './menu.repository';
-export * from './rol-menu.repository';
-export * from './rol.repository';
-export * from './usuario.repository';
-export * from './pqrs.repository';
+
